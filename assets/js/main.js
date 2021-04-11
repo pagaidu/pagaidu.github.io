b4bc3c377051e4f179f7d431976a6f2fe33a0d96
pagaidu <projekts.pagaidu@protonmail.com>
2021-04-11 14:24:34 +0300

    up the hill 17

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,11 @@
-
-
-const toggle = document.querySelector('.toggle');
+const toggle = document.querySelector('.menu-toggle');
 const submenu = document.getElementById('saturs');
-
-toggle.addEventListener('click', function(e) {
-   
+toggle.addEventListener('click', function(event) {
   submenu.classList.toggle('showSubMenu');
-      e.preventDefault();
+      event.preventDefault();
 });
 
-
-var arrow = document.querySelector('.menu-table-of-contents');
+const arrow = document.querySelector('.menu-table-of-contents');
 arrow.addEventListener('click', function(event) {
   event.target.classList.toggle('menu-table-of-contents-active');
 });
